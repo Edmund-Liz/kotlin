61a428d7d65a5fc55a9be9ab820c7e52071ab16d
Edmund-Liz <1836491284@qq.com>
2023-04-25 22:06:42 +0800

    4.25操作符重载，可空类型

---
+++ kotlin/src/main/kotlin/NullTest.kt
@@ -0,0 +1,22 @@
+package com.easy.kotlin
+
+fun strLength(s: String?):Int{
+//    s.length                  //编译不通过，不可对可空类型直接使用"."点操作符
+//    println(s!!.length)       //!!.断言操作符，此时若s为空将抛出空指针异常
+
+    return s?.length ?:0
+}
+
+fun main(args:Array<String>){
+    println(strLength(null))
+    println(strLength("abc"))
+
+    println(null==null)
+    println(null!=null)
+    println(null is Any)
+    println(null is Any?)
+//    var a=null
+//    a=1  编译不通过
+
+
+}
---
+++ kotlin/src/main/kotlin/operatorTest.kt
@@ -0,0 +1,47 @@
+package com.easy.kotlin
+
+
+data class Point(val x:Int,val y:Int)
+operator fun Point.unaryMinus()=Point(-x,-y)
+
+data class Counter(var index:Int)
+operator fun Counter.plus(increment:Int):Counter{
+    return Counter(index+increment)
+}
+
+data class Person(val name:String,var age:Int){
+    fun say(msg:String){
+        println("$name say:$msg")
+    }
+}
+infix fun Person.grow(year:Int):Person{
+    return Person(name,age+year)
+}
+
+fun what(){
+    println("This is WHAT?")
+}
+
+fun main(args:Array<String>){
+    val n=Point(1,1)
+    val np=-n
+    println(np)
+
+    var a=" "+1
+//    var b=1+""
+    var c=1.toString()+" "
+    println(a)
+    println(c)
+
+    val x=Counter(1)
+    val y=x+10
+    println(y)
+
+    val x1=true
+    val x2=x1?:"111"//?:操作符限定和null做比较，专门检查是否为空
+    println(x2)
+
+    val one=Person("liz",18)
+    val one1=one grow 10
+    println(one1)
+}
